06a47d03ecc0708fd0ed64d45cd3ebb4115967be
Max Vieweg <maximilian.manfred.vieweg@rwth-aachen.de>
2021-02-06 18:35:26 +0100

    added port listening and link to debug live setup

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const config = {
   port: 6299,
 };
 
 const server = express();
+server.listen(config.port);
 
-server.get("/", (req, res) => {
-  res.send("Hello World");
+server.get('/', (req, res) => {
+  res.send('Hello World');
 });
+
+// https://medium.com/better-programming/create-an-express-server-using-typescript-dec8a51e7f8d
