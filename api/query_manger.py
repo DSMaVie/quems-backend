835f72983acae0a0482a9c6de2f61cfae6ae8445
MaVie <47944114+DSMaVie@users.noreply.github.com>
2021-02-25 10:02:28 +0100

    started orm implementation (#5)
    Database class rework
    Fixes #3 ,Fixes #2

---
+++ api/query_manger.py
@@ -0,0 +1,129 @@
+from sqlalchemy import create_engine, text, select
+from sqlalchemy.orm import sessionmaker
+from pathlib import Path
+from schema import Event, Data, Place, Regularity, BaseTable
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+import os
+
+
+class QueryManager:
+    def __init__(self, path_to_db: Path):
+        # load and wrap initialization vars
+        debug = bool(os.getenv("DB_DEBUG", False))
+        need_dummy_data = bool(os.getenv("DB_FILL_WITH_DUMMIES", False))
+
+        # define engine and meta
+        db_url = "sqlite:///" + str(path_to_db.resolve())
+        self.engine = create_engine(db_url, echo=debug)
+        self.meta = BaseTable.metadata
+        self.session = sessionmaker(self.engine, future=True)
+
+        self.meta.create_all(bind=self.engine)
+
+        if need_dummy_data:
+            self.__load_dummy_data()
+
+    def add_singular_event(self, *args, **kwargs):
+        with self.session.begin() as sess:
+            if (place_id := Place.get_id(sess, name=kwargs["place"])) is None:
+                sess.add(Place(name=kwargs["place"]))
+                place_id = Place.get_id(sess, name=kwargs["place"])
+
+            data_kwargs = Data.drop_non_columns(kwargs)
+            sess.add(Data(**data_kwargs, place_id=place_id))
+            data_id = Data.get_id(sess, **data_kwargs)
+
+            event_kwargs = Event.drop_non_columns(kwargs)
+            sess.add(Event(**event_kwargs, data_id=data_id, created=dt.now()))
+            event_id = Event.get_id(sess, **event_kwargs)
+            return event_id
+
+    def add_template(self, *args, **kwargs):
+        with self.session.begin() as sess:
+            if (place_id := Place.get_id(sess, name=kwargs["place"])) is None:
+                sess.add(Place(name=kwargs["place"]))
+                place_id = Place.get_id(sess, name=kwargs["place"])
+
+            sess.add(Regularity(outdated=False))
+            reg_id = Regularity.get_id(sess, outdated=False)
+
+            data_kwargs = Data.drop_non_columns(kwargs)
+            sess.add(
+                Data(**data_kwargs, place_id=place_id, reg_id=reg_id)
+            )  # might need kwarg filtering
+            data_id = Data.get_id(sess, **data_kwargs)
+            return data_id
+
+    def add_regular_event(self, id, *args, **kwargs):
+        with self.session.begin() as sess:
+            sess.add(Event(data_id=id, **kwargs, created=dt.now()))
+
+    def __load_dummy_data(self):
+        s1_id = self.add_singular_event(
+            name_de="Mitgliederversammlung",
+            start=dt.now() + relativedelta(days=+1),
+            assignee="Vorstand",
+            place="Queerreferat",
+            desc_de="eine wilde Beschreibung taucht auf ...",
+            nl=True,
+            calendar=True,
+        )
+        s2_id = self.add_singular_event(
+            name_de="Workshop: ist queeres Python anders?",
+            name_en="Workshop: is queer python different?",
+            start=dt.now() + relativedelta(days=+2),
+            end=dt.now() + relativedelta(days=+2, hours=+2),
+            assignee="Alex",
+            place="Queerreferat",
+            desc_de="In diesem Workshop gehen wir der Frage nach, ob Python, dass von queeren Menschen geschrieben wird linguistisch different von hetero=python ist.",
+            insta=True,
+            nl=True,
+            calendar=True,
+        )
+        cafe_id = self.add_template(
+            name_de="Queercafe",
+            name_en="Queercafe",
+            desc_de="Kaffee und Kuchen",
+            assignee="Helga",
+            place="Online",
+            insta=True,
+            discord=True,
+        )
+        bm_id = self.add_template(
+            name_de="Vorstandssitzung",
+            desc_de="geschlossene Sitzung, nur fuer Vorstandsmitglieder",
+            place="Bei Jutta",
+            assignee="Jutta",
+        )
+        for index, id in enumerate([cafe_id, bm_id]):
+            self.add_regular_event(
+                id,
+                start=dt.now() + relativedelta(days=(index + 1) * 7),
+                end=dt.now() + relativedelta(days=(index + 1) * 7, hours=+2),
+            )
+            self.add_regular_event(
+                id,
+                start=dt.now() + relativedelta(days=(index + 2) * 7),
+                end=dt.now() + relativedelta(days=(index + 2) * 7, hours=+2),
+            )
+
+    def get_all_events(self):
+        with self.session.begin() as sess:
+            stmt = text(
+                "SELECT * FROM (events JOIN data ON (data.id == events.data_id)) JOIN places ON (data.place_id = places.id)"
+            )
+            result = sess.execute(stmt)
+            return [dict(row) for row in result]
+
+    def get_all_templates(self):
+        with self.session.begin() as sess:
+            stmt = text("SELECT * FROM data JOIN places ON (data.place_id = places.id)")
+            result = sess.execute(stmt).scalars()
+            return [dict(row) for row in result]
+
+    def get_all_places(self):
+        with self.session.begin() as sess:
+            stmt = select(Place.name)
+            result = sess.execute(stmt).scalars()
+            return list(result)
--- api/schema.py
+++ api/schema.py
@@ -1,67 +1,64 @@
-from typing import Union, Dict
-from sqlalchemy.orm import declacrative_base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-
-Base = declacrative_base()
+from sqlalchemy.orm import as_declarative
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, select
 
 
-class Event(Base):
-    __tablename__ = "events"
-
+@as_declarative()
+class BaseTable:
     id = Column(Integer, primary_key=True)
+
+    @classmethod
+    def get_id(cls, session, **kwargs):
+        stmt = select(cls.id).filter_by(**kwargs)
+        result = session.execute(stmt)
+        return result.scalar()
+
+    @classmethod
+    def drop_non_columns(cls, dict):
+        cols = cls.__table__.columns.keys()
+        return {key: dict[key] for key in dict if key in cols}
+
+    def to_dict(self):
+        cols = self.__table__.columns.keys()
+        return {key: getattr(self, key) for key in cols}
+
+
+class Event(BaseTable):
+    __tablename__ = "events"
+    # TODO: sqlalchemy has datetime datatype which is represented as string buttype checked for datetime python obj before insertion
     start = Column(Integer, nullable=False)
     end = Column(Integer)
     data_id = Column(Integer, ForeignKey("data.id"), nullable=False)
-    created = Column(Integer, nullable=False)
+    created = Column(Integer, nullable=False)  # when using datetime set default to now
     last_edited = Column(Integer)
 
-    @classmethod
-    def get_event(cls, session, event: Dict[str, int]):
-        raise NotImplementedError
 
-
-class Data(Base):
+class Data(BaseTable):
     __tablename__ = "data"
 
-    id = Column(Integer, primary_key=True)
     place_id = Column(Integer, ForeignKey("places.id"), nullable=False)
-    reg_id = (Column(Integer, ForeignKey("regularities.id")),)
+    reg_id = Column(Integer, ForeignKey("regularities.id"))
     assignee = Column(String, nullable=False)
     name_de = Column(String, nullable=False)
     name_en = Column(String)
     desc_de = Column(String)
     desc_en = Column(String)
-    fb = Column(Boolean, nullable=False)
-    insta = Column(Boolean, nullable=False)
-    twitter = Column(Boolean, nullable=False)
-    discord = Column(Boolean, nullable=False)
-    nl = Column(Boolean, nullable=False)
-    calendar = Column(Boolean, nullable=False)
-
-    @classmethod
-    def get_id(cls, session, data: Dict[str, Union[str, int, bool]]):
-        raise NotImplementedError
+    fb = Column(Boolean, nullable=False, default=False)
+    insta = Column(Boolean, nullable=False, default=False)
+    twitter = Column(Boolean, nullable=False, default=False)
+    discord = Column(Boolean, nullable=False, default=False)
+    nl = Column(Boolean, nullable=False, default=False)
+    calendar = Column(Boolean, nullable=False, default=False)
 
 
-class Place(Base):
+class Place(BaseTable):
     __tablename__ = "places"
 
-    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    @classmethod
-    def get_id(cls, session, place_name):
-        raise NotImplementedError
 
-    @classmethod
-    def get_place(cls, session, place_id):
-        raise NotImplementedError
-
-
-class Regularity(Base):
+class Regularity(BaseTable):
     __tablename__ = "regularities"
 
-    id = Column(Integer, primary_key=True)
     outdated = Column(Boolean, nullable=False)
     ref_event = Column(Integer, ForeignKey("data.id"))
     ref_offset = Column(Integer)
@@ -70,9 +67,4 @@ class Regularity(Base):
     )  # 0 -> every week, #1 -> every first [weekday], #2 -> every second [weekday], # -1 -> every last [weekday]
     reg_weekday = Column(Integer)
 
-    # TODO: contraints for regularity params
-
-    @classmethod
-    def get_last_id(cls, session):
-        # for now this is enough. needs more complex getter logic later
-        raise NotImplementedError
+    # TODO: constraints for regularity params
