4509184207ec3428f0c3a7308aa3706b0bba5b08
Max Vieweg <maximilian.manfred.vieweg@rwth-aachen.de>
2021-03-29 21:20:37 +0200

    added parametrized test

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,13 +1,17 @@
 from api.db.schema import Event, Data, Place, Regularity
+import pytest
 
 
-def test_place():
-    dummy_data = {"name": "Queerreferat", "not_a_col": "not_a_col"}
-    place_data = Place.drop_non_columns(dummy_data)
+@pytest.mark.parametrize(
+    "schema_cls,dummy_data",
+    [(Place, {"name": "testname"}), (Regularity, {"outdated": True})],
+)  # , need to fill with data)
+def test_schema(schema_cls, dummy_data):
+    dummy_data |= {"not_a_col": None}
+    cleaned_data = schema_cls.drop_non_columns(dummy_data)
+    assert "not_a_col" not in cleaned_data.keys()
 
-    assert "not_a_col" not in place_data.keys()
-    place = Place(**place_data)
+    schema_obj = schema_cls(**cleaned_data)
+    extracted_data = schema_obj.to_dict()
 
-    extracted_data = place.to_dict()
-    del extracted_data["id"]
-    assert extracted_data == place_data
+    assert extracted_data.items() >= cleaned_data.items()
